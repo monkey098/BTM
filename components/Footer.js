6c14b37ddc11ca61708001d81cd67799c633a3bc
monkey098 <158118711+monkey098@users.noreply.github.com>
2025-09-04 16:41:58 +0200

    Updates

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <>
       <footer className={styles.footer}>
-        <img src="/logo-netlify.svg" alt="Netlify Logo" className={styles.logo} />
+        <img src="/TCT.png" alt="TheCalTra Logo" className={styles.logo} />
       </footer>
     </>
   )
